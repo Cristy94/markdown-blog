c2246b099c55a4ad883b426f6153a8c70d4b965c
Buleandra Cristian <cristian.buleandra@gmail.com>
2024-03-20 01:19:08 +0100

    Fix path traversal issue. Add 404 header to 404 pages.

--- markdown-blog/blog-page.php
+++ markdown-blog/blog-page.php
@@ -4,11 +4,17 @@
  */
 require_once 'postRenderer.php';
 $postSlug = $_GET['page'];
-$page = 'posts/' . $postSlug;
-if (file_exists($page)) {
+$page = __DIR__ . '/posts/' . $postSlug;
+
+// Avoid path traversal, if /../ is passed
+$realPath = realpath($page);
+$containsPathTraversal = $realPath === false || strpos($realPath, __DIR__) !== 0;
+
+if (!$containsPathTraversal && file_exists($page)) {
     $markdown = file_get_contents($page);
     $pageTitle = getPostTitle($markdown);
 } else {
+    header('HTTP/1.1 404 Not Found');
     $markdown = "# <center>404 😢<br/> Post not found<br/></center>";
     $markdown.= '<center><a href="./">Back to all posts</a></center>';
     $pageTitle = 'Blog post not found!';
